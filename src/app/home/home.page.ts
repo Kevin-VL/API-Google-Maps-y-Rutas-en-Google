a043a8e94aecdf266dab9275970373161b1c3a0f
Kevin-VL <yarikalvarez@gmail.com>
2021-08-03 08:23:36 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var google;
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  map = null;
 
   constructor() {}
-
+  ngOnInit() {
+    this.loadMap();
+  }
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: 4.658383846282959, lng: -74.09394073486328};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      //this.renderMarkers();
+      mapEle.classList.add('show-map');
+    });
+  }
+  
 }
